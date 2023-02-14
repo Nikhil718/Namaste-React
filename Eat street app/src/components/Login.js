d8b6d67460c94e3dfc831f86bad22e84e77c4e38
Nikhil <Naiknikhil925@gmail.com>
2023-02-15 01:12:09 +0530

    login page updated

--- Eat street app/src/components/Login.js
+++ Eat street app/src/components/Login.js
@@ -58,7 +58,7 @@ const Login = () => {
                       value={values.email}
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="name@gmail.com"
-                      required=""
+                      required
                     />
                     {errors.email && touched.email && errors.email}
                   </div>
@@ -78,7 +78,7 @@ const Login = () => {
                       value={values.password}
                       placeholder="••••••••"
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      required=""
+                      required
                     />
                     {errors.password && touched.password && errors.password}
                   </div>
